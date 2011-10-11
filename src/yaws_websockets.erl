7b16084ce026c500e58a7aaa04625ec67667109a
Andre Hilsendeger <andre.hilsendeger@googlemail.com>
2011-10-11 10:52:08 +0200

    Patched yaws_1.89 with the websocket hy10 support from jbothma

--- src/yaws_websockets.erl
+++ src/yaws_websockets.erl
@@ -14,12 +14,16 @@
 -include("yaws_debug.hrl").
 
 -include_lib("kernel/include/file.hrl").
--export([handshake/3, unframe_one/1, unframe_all/2]).
+-export([handshake/3, unframe_one/2, unframe_all/3, frame/2]).
 
 handshake(Arg, ContentPid, SocketMode) ->
     CliSock = Arg#arg.clisock,
+    %jdtodo io:format("CliSock ~p~n",[CliSock]),
     case get_origin_header(Arg#arg.headers) of
 	undefined ->
+	    %% TODO: Lack of origin header is allowed for non-browser clients 
+            %% in hybi 17 but for simplicity the connection is closed for now.
+	    %jdtodo io:format("No origin header.~n"),
 	    %% Yaws will take care of closing the socket
 	    ContentPid ! discard;
 	Origin ->
@@ -49,7 +53,7 @@ handshake(Arg, ContentPid, SocketMode) ->
 	    end,
 	    case TakeOverResult of
 		ok ->
-		    ContentPid ! {ok, CliSock};
+		    ContentPid ! {ok, CliSock, ProtocolVersion};
 		{error, Reason} ->
 		    ContentPid ! discard,
 		    exit({websocket, Reason})
@@ -57,7 +61,8 @@ handshake(Arg, ContentPid, SocketMode) ->
     end,
     exit(normal).
 
-handshake(ws_76, Arg, CliSock, WebSocketLocation, Origin, Protocol) ->
+handshake(hi_76, Arg, CliSock, WebSocketLocation, Origin, Protocol) ->
+    io:format("hi_75!~n",[]),
     {ok, Challenge} = gen_tcp:recv(CliSock, 8),
     Key1 = secret_key("sec-websocket-key1", Arg#arg.headers),
     Key2 = secret_key("sec-websocket-key2", Arg#arg.headers),
@@ -70,23 +75,59 @@ handshake(ws_76, Arg, CliSock, WebSocketLocation, Origin, Protocol) ->
      "Sec-WebSocket-Protocol: ", Protocol, "\r\n",
      "\r\n", ChallengeResponse];
 
-handshake(ws_75, _Arg, _CliSock, WebSocketLocation, Origin, _Protocol) ->
+handshake(hi_75, _Arg, _CliSock, WebSocketLocation, Origin, _Protocol) ->
+    io:format("hi_75!~n",[]),
     ["HTTP/1.1 101 Web Socket Protocol Handshake\r\n",
      "Upgrade: WebSocket\r\n",
      "Connection: Upgrade\r\n",
      "WebSocket-Origin: ", Origin, "\r\n",
      "WebSocket-Location: ", WebSocketLocation, "\r\n",
+     "\r\n"];
+
+handshake(_Ver, Arg, _CliSock, _WebSocketLocation, _Origin, _Protocol) ->
+    io:format("10!~n",[]),
+    Key = get_nonce_header(Arg#arg.headers),
+    AcceptHash = hash_nonce(Key), 
+    ["HTTP/1.1 101 Switching Protocols\r\n",
+     "Upgrade: websocket\r\n",
+     "Connection: Upgrade\r\n",
+     "Sec-WebSocket-Accept: ", AcceptHash , "\r\n",
      "\r\n"].
 
 ws_version(Headers) ->
+    %io:format("IDing Websocket Version...~n~p~n",[Headers]),
+    case query_header("sec-websocket-version", Headers) of
+	"8" -> 8;
+	"17" -> 17;
+	_ ->
     case query_header("sec-websocket-key1", Headers) of
-	undefined ->  ws_75;
-	_         ->  ws_76
+		undefined -> 
+		    %% might be hixie 75 but...
+		    %% TODO: should really negotiate a hybi version here
+		    hi_75; 
+		_ -> hi_76
+	    end
     end.
+
+unframe_one(8, DataFrames) ->
+    <<1:1,_Rsv:3,_Opcode:4,1:1,Len1:7,Rest/binary>> = DataFrames,
+
+    case Len1 of
+	126 ->
+	    <<Len:16, MaskingKey:4/binary, Payload:Len/binary>> = Rest;
+	127 ->
+	    <<Len:64, MaskingKey:4/binary, Payload:Len/binary>> = Rest;
+	Len ->
+	    <<_:0, MaskingKey:4/binary, Payload:Len/binary>> = Rest
+    end,
+    UnmaskedData = mask(1, binary_to_list(MaskingKey), Payload),
+
+    {ok, list_to_binary(UnmaskedData), <<>>}; %jdtodo baaaaad
 
 %% This should take care of all the Data Framing scenarios specified in
 %% http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol-66#page-26
-unframe_one(DataFrames) ->
+unframe_one(hi_75, DataFrames) -> unframe_one(hi_76, DataFrames);
+unframe_one(hi_76, DataFrames) ->
     <<Type, _/bitstring>> = DataFrames,
     case Type of
 	T when (T =< 127) ->
@@ -108,19 +149,67 @@ unframe_one(DataFrames) ->
 	    {ok, Data, NextFrame}
     end.
 
-unframe_all(<<>>, Acc) ->
+unframe_all(_, <<>>, Acc) ->
     lists:reverse(Acc);
-unframe_all(DataFramesBin, Acc) ->
-    {ok, Msg, Rem} = unframe_one(DataFramesBin),
-    unframe_all(Rem, [Msg|Acc]).
+unframe_all(ProtocolVersion, DataFramesBin, Acc) ->
+    {ok, Msg, Rem} = unframe_one(ProtocolVersion, DataFramesBin),
+    unframe_all(ProtocolVersion, Rem, [Msg|Acc]).
+
+frame(hi_75, IoList) ->
+    frame(hi_76, IoList);
+frame(hi_76, IoList) ->
+    [0, IoList, 255];
+frame(_, Data) ->
+    %FIN=true because we're not fragmenting.
+    %OPCODE=1 for text
+    FirstByte = 128 bor 1,
+    ByteList = binary_to_list(Data),
+    Length = length(ByteList),
+    if
+	Length =< 126 ->
+	    << FirstByte, 0:1, Length:7, Data:Length/binary >>;
+	Length =< 65535 ->
+	    << FirstByte, 0:1, 126:7, Length:16, Data:Length/binary >>;
+	true ->
+	    Defined = Length =< math:pow(2,64),
+	    % TODO: Is the correctness of this pow call 
+	    % better than the speed and danger of not checking?
+	    case Defined of
+		true ->
+		    << FirstByte, 0:1, 127:7, Length:16, Data:Length/binary >>;
+		_ ->
+		    undefined
+	    end
+    end.
+
+%%  
+%% mask(Pos, Mask, Data)
+%%
+%% N: integer, the position in the masking key to use.
+%% MaskingKey: list of 8-bit integers
+%% Data: bit string divisible by 8
+%%
+%% This could probably be done better but seems to work for now.
+%%
+mask(_, _, <<>>) -> [];
+mask(5, MaskingKey, Data) -> mask(1, MaskingKey, Data);
+mask(N, MaskingKey, <<Head:8/integer,Rest/binary>>) ->
+    Masked = lists:nth(N, MaskingKey) bxor Head,
+    [Masked | mask(N + 1, MaskingKey, Rest)].
 
 
 %% Internal functions
 get_origin_header(Headers) ->
-    query_header("origin", Headers).
+    case query_header("origin", Headers) of
+	undefined -> query_header("sec-websocket-origin", Headers);
+	Origin    -> Origin
+    end.
 
 get_protocol_header(Headers) ->
     query_header("sec-websocket-protocol", Headers, "unknown").
+
+get_nonce_header(Headers) ->
+    query_header("sec-websocket-key", Headers).
 
 query_header(HeaderName, Headers) ->
     query_header(HeaderName, Headers, undefined).
@@ -164,6 +253,11 @@ digits32(Num) ->
     Digit2 = Num3 rem 256,
     Digit1 = Num3 div 256,
     [Digit1, Digit2, Digit3, Digit4].
+
+hash_nonce(Nonce) ->
+    Salted = Nonce ++ "258EAFA5-E914-47DA-95CA-C5AB0DC85B11",
+    HashBin = crypto:sha(Salted),
+    base64:encode_to_string(HashBin).
 
 unpack_length(Binary, LenBytes, Length) ->
     <<_, _:LenBytes/bytes, B, _/bitstring>> = Binary,
